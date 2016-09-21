d37529aa88f673b9bd5fc9a048fd34f30e91bd3e
Nicolas Pelletier <npelletier@wivora.fr>
2016-09-21 21:56:13 +0200

    feat(Yolo): allow fqn to have arguments, fields and data paths for traversal

--- test/yolo.js
+++ test/yolo.js
@@ -5,38 +5,38 @@ describe('Yolo', function () {
 
   describe('Fqn', function () {
 
-    [ { fqn: 'Absolute.Path.To.A.Child', has: 'type', eq: 'root' }
-    , { fqn: '.Relative.Path.To.A.Child', has: 'next.next.name', eq: 'To' }
-    , { fqn: 'Child.With:inlet', has: 'next.next.next.name', eq: 'inlet' }
-    , { fqn: ':inlet-without-path', has: 'type', eq: 'inlet' }
-    , { fqn: 'Child.With[data . field]', has: 'next.next.next.path', eq: 'data.field', nostr: true }
-    , { fqn: '[data.field.without.path]', has: 'type', eq: 'data' }
-    , { fqn: '.Child.With.Model/and/field/and/subfield', has: 'next.next.next.type', eq: 'field' }
-    , { fqn: '.Path.To.Model<.With/path.Paramerter>', has: 'next.next.args.0.next.type', eq: 'field' }
-    , { fqn: '.Model<A,[with.data.parameter]>.And.SubChild', has: 'args.1.type', eq: 'data' }
-    , { fqn: '.Model<With[mixed],[parameter]>', has: 'args.0.next.next.path', eq: 'mixed' }
+    [ { fqn: 'Absolute.Path.To.A.Child', has: '0.type', eq: 'root' }
+    , { fqn: '.Relative.Path.To.A.Child', has: '2.name', eq: 'To' }
+    , { fqn: 'Child.With:inlet', has: '3.name', eq: 'inlet' }
+    , { fqn: ':inlet-without-path', has: '0.type', eq: 'inlet' }
+    , { fqn: 'Child.With[data . field]', has: '3.path', eq: 'data.field', nostr: true }
+    , { fqn: '[data.field.without.path]', has: '0.type', eq: 'data' }
+    , { fqn: '.Child.With.Model/and/field/and/subfield', has: '3.type', eq: 'field' }
+    , { fqn: '.Path.To.Model<.With/path.Paramerter>', has: '2.args.0.1.type', eq: 'field' }
+    , { fqn: '.Model<A,[with.data.parameter]>.And.SubChild', has: '0.args.1.0.type', eq: 'data' }
+    , { fqn: '.Model<With[mixed],[parameter]>', has: '0.args.0.2.path', eq: 'mixed' }
     ].map(function (e) {
       it('should parse: ' + e.fqn, function () {
-        var fqn = new Yolo.Fqn(e.fqn);
+        var fqn = Yolo.Fqn.parse(e.fqn);
         var path = e.has.split('.');
         var ii = fqn;
-        /*
+        /**/
         try {
-        */
+        /**/
         for (var i = 0; i < path.length; i++)
           ii = ii[path[i]];
         assert.equal(ii, e.eq);
-        /*
+        /**/
         } catch (f) {
           console.log(e.fqn, require('util').inspect(fqn, null, 10));
           process.exit();
         }
-        */
+        /**/
         return ;
       });
       if (e.nostr != true) {
         it('sould serialize: ' + e.fqn, function () {
-          assert.equal(new Yolo.Fqn(e.fqn).toString(), e.fqn);
+          assert.equal(Yolo.Fqn.stringify(Yolo.Fqn.parse(e.fqn)), e.fqn);
         });
       }
     });
