d3d9af5014eb40510c83ccc8e8585b35d99b0263
Nicolas Pelletier <npelletier@wivora.fr>
2016-10-07 00:46:56 +0200

    fix(Yolo): fix infinite loop on inlet processing

--- test/yolo-inlets.js
+++ test/yolo-inlets.js
@@ -17,12 +17,13 @@ describe('Yolo', function () {
       var C = new Yolo.Node('C');
       A.setOlder(B);
       B.setOlder(C);
-      A.on('test1', 'prepare', function (e, c) { c(null, [2]) });
-      C.on('test1', 'prepare', function (e, c) { c(null, [4]) });
-      B.on('test1', function (e, c) { c(null, e.concat(7)); });
+      A.on('test1', 'prepare', function (e, c) { c(null, [1]) });
+      C.on('test1', 'prepare', function (e, c) { c(null, [2]) });
+      B.on('test1', function (e, c) { c(null, e.concat(4)); });
+      C.on('test1', 'format', function (e, c) { c(null, e.concat(7)); });
       B.on('test1', 'format', function (e, c) { c(null, e.sum()); });
       A.on('test1', 'format', function (e, c) { c(null, e * 3); });
-      A.send(':test1', [1], function (err, result) {
+      A.send(':test1', [], function (err, result) {
         if (err) return done(err);
         try { assert.equal(result, 42); }
         catch (e) { return done(e); }
@@ -42,8 +43,9 @@ describe('Yolo', function () {
       C.on('test2', 'error', function (e, c) { c('should never execute'); });
       B.on('test2', 'error', function (e, c) { c(e || 'did not receive error'); });
       A.on('test2', 'format', function (e, c) { c('should never execute'); });
-      A.send(':test1', 'some', function (err) {
-        if (err != 'this is an error') return done('Return an unexprected value');
+      A.send(':test2', 'some', function (err) {
+        if (err != 'this is an error')
+          return done('return an unexprected value: ' + JSON.stringify(arguments));
         return done();
       });
     });
