cde637233abb74e3d602b964a9306b0b47184de2
Nicolas Pelletier <npelletier@wivora.fr>
2016-10-17 20:28:31 +0200

    test(Yolo.Inlets): adding test for synchronous calls

--- test/yolo-inlets.js
+++ test/yolo-inlets.js
@@ -50,6 +50,26 @@ describe('Yolo', function () {
       });
     });
 
+    (function () {
+      var A = new Yolo.Node('A');
+      A.on('test3', function (payload) { throw new Error('will be catched'); });
+      A.on('test4', function (payload) { return 42; });
+
+      it('should works with synchronous function (success)', function (done) {
+        A.send(':test3', {}, function (err) {
+          if (err) return done();
+          else return done('An error was expected');
+        });
+      });
+
+      it('should works with synchronous function (error)', function (done) {
+        A.send(':test4', {}, function (err, res) {
+          return done(res == 42 ? null : 'expected result was 42 got ' + res);
+        });
+      });
+
+    })();
+
   });
 
 });
