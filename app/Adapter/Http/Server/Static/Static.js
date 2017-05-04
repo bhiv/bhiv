e6b458a4f4feb01b10c3223842f9a8359e0c7fd7
Nicolas Pelletier <npelletier@wivora.fr>
2017-05-04 17:09:26 +0200

    refactor(Adapter.Http): moved to his own repository

--- app/Adapter/Http/Server/Static/Static.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (node, logger, Bee) {
-
-  node.on('serve', new Bee()
-          .pipe({ type: 'file', filepath: 'jp:[`static/`,params.filepath,params."0"]|join(``,@)' })
-          .end()
-         );
-
-  return node;
-};
